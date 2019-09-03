1c68b979e6dbaba93d28a539cae7c79d6babc5eb
Timgriffin1 <timgriffin1@gmail.com>
2019-09-03 13:19:15 +0000

    blur.rb

--- app/views/users/blur.rb
+++ app/views/users/blur.rb
@@ -12,11 +12,42 @@ class Image
       row.each do |pixel|
         print pixel, ' '
       end 
-      puts  
+      puts    
     end 
   end 
 
+  def blur(row_index, col_index)
+    @array[row_index -1][col_index] = 1 unless row_index == 0
+    @array[row_index +1][col_index] = 1 unless row_index >= @row_length-1
+    @array[row_index][col_index -1] = 1 unless col_index == 0
+    @array[row_index][col_index +1] = 1 unless col_index >= @col_length-1
   end
+
+  def find_ones
+    one_pixel = []
+
+    @array.each_with_index do |row_array, row_index|
+        row_array.each_with_index do |cell, col_index|
+        if cell == 1
+          one_pixel << [row_index, col_index]
+        end
+      end
+    end    
+    one_pixel  
+  end
+
+
+  def transform
+    array_coords = find_ones
+
+    array_coords.each do |x, y|
+      blur(x, y)
+    end  
+  end
+
+
+end
+
 
 image = Image.new([
   [0, 0, 0, 0],
@@ -25,4 +56,7 @@ image = Image.new([
   [0, 0, 0, 0]
 ])
 
+image.output_image
+puts "----"
+image.transform
 image.output_image
